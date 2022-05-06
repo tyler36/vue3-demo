5f1bdb7d074159542e7ff90338cf219359c4f1fc
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-05-06 09:58:36 +0900

    component props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,10 +1,25 @@
 export default {
-    template: `
-        <button :disabled="processing" class="px-5 py-2 bg-gray-200 border rounded disabled:cursor-not-allowed hover:bg-gray-400"><slot /></button>
+    template: /*html*/`
+        <button
+            :class="{
+                'px-5 py-2 border rounded disabled:cursor-not-allowed': true,
+                'bg-blue-600 hover:bg-blue-700': type === 'primary',
+                'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+                'bg-gray-200 hover:bg-gray-400': type === 'muted',
+                'is-loading': 'processing',
+            }"
+            :disabled="processing" >
+                <slot />
+            </button>
     `,
-    data() {
-        return {
-            processing: false,
-        };
-    },
+    props: {
+        processing: {
+            type: Boolean,
+            default: false
+        },
+        type: {
+            type: String,
+            default: 'primary'
+        },
+    }
 }
