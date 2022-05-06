dffb8f00b2d9e6809e0a3649dd5dd2f8fe00ef90
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-05-06 11:18:27 +0900

    handle a form submit

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -9,6 +9,13 @@ export default {
         <div class="space-y-8">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+            <form @submit.prevent="add">
+                <div className="text-black border border-gray-600">
+                    <input v-model="newAssignment" placeholder="New Assignment ..." class="p-2">
+                    <button type="submit" class="p-2 text-white border-left">Add</button>
+                </div>
+            </form>
         </div>
     `,
 
@@ -18,10 +25,22 @@ export default {
                 {id: 1, name: 'Finish project', complete: false},
                 {id: 2, name: 'Read Chapter 4', complete: false},
                 {id: 3, name: 'Turn in Homework', complete: false},
-            ]
+            ],
+            newAssignment: '',
         }
     },
 
+    methods: {
+        add() {
+            this.assignments.push({
+                id:       this.assignments.length + 1,
+                name:     this.newAssignment,
+                complete: false,
+            });
+
+            this.newAssignment = '';
+        }
+    },
 
     computed: {
         filters() {
