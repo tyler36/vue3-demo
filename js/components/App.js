35cd0e53f290df3d1e7a0c15e86604d897d5849e
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-05-20 09:57:25 +0900

    Named Slots

--- js/components/App.js
+++ js/components/App.js
@@ -1,9 +1,38 @@
 import Assignments from './Assignments.js';
+import Panel from './Panel.js';
 
 export default {
     components: {
-        Assignments
+        Assignments,
+        Panel,
     },
 
-    template: /*html*/`<Assignments></Assignments>`,
+    template: /*html*/`
+    <div class="grid gap-4">
+        <Assignments></Assignments>
+        <Panel>
+            Default content
+        </Panel>
+
+        <Panel>
+            <template v-slot:heading>
+                Title
+            </template>
+
+            Default content
+        </Panel>
+
+        <Panel theme="light">
+            <template v-slot:heading>
+                Title
+            </template>
+
+            Default content
+
+            <template v-slot:footer>
+                Here be dragons!
+            </template>
+        </Panel>
+    </div>
+    `,
 }
---
+++ js/components/Panel.js
@@ -0,0 +1,26 @@
+export default {
+    template: /*html*/`
+        <div :class="{
+                'p-4 border rounded-lg': true,
+                'bg-gray-700 border-gray-600 text-white': theme === 'dark',
+                'bg-white border-gray-300 text-gray-900': theme === 'light',
+            }">
+            <h2 v-if="$slots.heading" class="font-bold mb-2">
+                <slot name="heading"/>
+            </h2>
+
+            <slot />
+
+            <footer v-if="$slots.footer" class="border-t mt-4 pt-4 border-gray-600 text-sm">
+                <slot name="footer"/>
+            </footer>
+        </div>
+    `,
+
+    props: {
+        theme: {
+            type: String,
+            default: 'dark',
+        }
+    }
+}
